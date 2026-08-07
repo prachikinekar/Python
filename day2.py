a5d64c0092539686e1fb7fdd2468a666ad867f8e
DESKTOP-MPJJ6C3\Kinekar Vaishnavi <vaishnavikinekar11062@gmail.com>
2026-08-07 13:28:34 +0530

    addday2.py

---
+++ day2.py
@@ -0,0 +1,73 @@
+#intialization
+#while(condition):
+# statement
+# inc/dec
+
+# program 1:
+# print 1 to 3
+
+# i1=1
+# while(i1<=3):
+#     print(i1)#1 #2 #3
+#     i1=i1+1 #2 #3
+
+# # program 2:
+# # print 1 to 5
+# i2=1
+# while(i2<=5):
+#     print(i2)
+#     i2=i2+1
+
+# # print hello 3 times
+# i3=1
+# while i3<=3:
+#     print("hello")
+#     i3=i3+1
+
+
+# # print 5 to 1
+# i4=5
+# while i4>=1:
+#     print(i4)
+#     i4=i4-1
+
+# print table of 2
+# i1=1
+# while i1<=10:
+#     print(2*i1)
+#     i1=i1+1
+
+# i2=2
+# while i2<=20:
+#     print(i2)
+#     i2=i2+2
+
+# table of 3 reverse
+# i3=30
+# while i3>=3:
+#     print(i3)
+#     i3=i3-1
+    
+# # table of 5 in reverse
+# i4=10
+# while i4>=1:
+#     print(i4*5)
+#     i4=i4-1
+
+
+# break statement with while loop
+# q=1
+# while(q<=5):
+#     if(q==3):
+#         break
+#     print(q)
+#     q=q+1
+
+# continue with while loop
+x=1
+while(x<=5):
+    if x==3:
+        x=x+1
+        continue
+    print(x)#1 2 4 5
+    x=x+1
