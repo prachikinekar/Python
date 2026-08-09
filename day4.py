6aec7c585f8e70b560917d9c35821c9125df4f08
DESKTOP-MPJJ6C3\Kinekar Vaishnavi <vaishnavikinekar11062@gmail.com>
2026-08-09 18:14:13 +0530

    add day4.py

---
+++ day4.py
@@ -0,0 +1,48 @@
+# Methods of list
+# append
+# names=["sarika","Ram","Sham"]
+# names.append("prachi")
+# print(names)
+
+# # pop
+# fruits=["orange","Mango","Banana","Grapes"]
+# fruits.pop()
+# print(fruits)
+
+# program 3
+# fruits=["orange","Mango","Banana","Grapes"]
+# fruitsB=["papya","Litchi"]
+# fruits.extend(fruitsB)
+# print(fruits)
+
+
+# program 5
+# cities=["pune","Mumbai","Banglore","Kolkata","Rajasthan","pune"]
+# e3=cities.count("pune")
+# print(e3)
+
+# # clear
+# country = ["india","pakistan","srilanka","bangladesh"]
+# print(country)
+# country.clear()
+# print(country)
+
+
+# reverse
+city=["Katol","Nagpur","Mumbai","Pune"]
+# city.reverse()
+# print(city)
+
+# sort
+city=["Katol","Nagpur","Mumbai","Pune"]
+city.sort()
+city.reverse()
+print(city)
+
+
+# program 6
+country=["India","Pakistan","Srilanka","Bangladesh"]
+country.insert(2,"China")
+print(country)
+
+# list ,dict ,tuple ,string
