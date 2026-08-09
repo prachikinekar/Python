e53911542d40a5c67b8e695ffb062c2497a924eb
DESKTOP-MPJJ6C3\Kinekar Vaishnavi <vaishnavikinekar11062@gmail.com>
2026-08-09 18:12:41 +0530

    added

---
+++ day3.py
@@ -0,0 +1,55 @@
+# # listA
+# names=["prachi","vaish","krish"]
+# print(names)
+# print(type(names))
+
+# numbers=[11,22,33,44]
+# print(numbers)
+# print(type(numbers))
+
+# info=["prachi","kinekar",22,49,True]
+# print(info)
+# print(type(info))
+
+# # program 2
+# # 
+# cities=["Pune","Mumbai","Banglore","Kolkata"]
+# print(cities[0])
+# print(cities[1])
+# print(cities[2])
+# print(cities[3])
+
+# e=len(cities)
+# print(e)
+# lastindex=len(cities)-1
+# print(lastindex)
+
+# # program 2 
+# countries=["India","Pakistan","Srilanka","Cuba","China"]
+# print(countries)
+
+# # for loop
+# #for x in range(startIndex,endIndex,stepSize):
+#     # print(x)
+
+# for x in range(0,5,1):
+#     print(x)
+
+
+# program 3:
+flowers=["Rose","lily","jasmine","sunflower","Lotus"]
+for x in range(len(flowers)):
+    # print(x)
+    print(flowers[x])
+
+animals=["Tiger","Lion","Rabbit"]
+print(animals)
+print(len(animals))
+
+i0=0
+while i0<=len(animals)+1:
+    print(i0)
+    print(animals[i0])
+    i0=i0+1# 1 2 3 
+
+
