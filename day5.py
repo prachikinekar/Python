a3fa0400852beb4e70ed17ad40ae85b55406f2e9
DESKTOP-MPJJ6C3\Kinekar Vaishnavi <vaishnavikinekar11062@gmail.com>
2026-08-10 12:26:14 +0530

    add file

---
+++ day5.py
@@ -0,0 +1,62 @@
+# listA=[11,22,33]
+# print(listA)
+# listB=listA#same memory so if we make change in one there will be change in other 
+# listB[0]=111
+# print(listA)#[111,22,33]
+# print(listB)#[111,22,33]
+
+
+# listA=[11,22,33]
+# listC=listA.copy()
+# listC[0]=111
+# print(listA)
+# print(listC)
+
+
+# # append()
+# names=["sarika","amol","Ram","Shyam","Snehal","Dhyan"]
+# names.append("satish")
+# print(names)
+
+# names.clear()
+# print(names)
+
+# # sort
+# cities=["Ramtek","Nagpur","Katol","Rajasthan"]
+# cities.sort()
+# print(cities)
+
+# # reverse()
+# cities.reverse()
+# print(cities)
+
+# # extend 
+# numA=[11,22,33,44,55]
+# numB=[66,77,88,99]
+# numA.extend(numB)#adds the ele to num A
+# print(numA)
+# print(numB)
+
+# # remove pop
+# country=["India","Srilanka","China","cuba"]
+# country.pop()
+# country.remove("China")
+# print(country)
+
+# country=["India","Srilanka","China","cuba"]
+# country.insert(2,"Pakistan")
+# print(country)
+
+# # index
+# country=["India","srilanka","china","cuba","india","US","Uk",'Mexico',"india"]
+# # del country
+# print(country)
+# print(len(country))
+
+# numA=[11,33,44,66,77765]
+# print(min(numA))#11
+# print(max(numA))#77765
+
+names=["Prachi","Krish","Shreya"]
+print("Prachi" in names)#true
+
