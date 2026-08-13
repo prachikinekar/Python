323528c81027304f6a1c64f3d4118eaf67b38cce
DESKTOP-MPJJ6C3\Kinekar Vaishnavi <vaishnavikinekar11062@gmail.com>
2026-08-13 13:58:36 +0530

    add day8.py

---
+++ day8.py
@@ -0,0 +1,49 @@
+a="Prachi"
+print(a)
+print(type(a))
+
+b="kinekar"
+print(b)
+print(type(b))
+
+
+c="""
+hello i am learning python
+python is good language for AI
+"""
+print(c)
+print(type(c))
+
+c='''
+  I am learning python and 
+    python is go to language for AI
+'''
+
+print(type(c))
+print(c)
+
+# program 2
+names=["Prachi","Krish","Vaishnavi"]
+print(names[0])
+names[0]="Rachi"#update
+print(names)
+
+# str immutable-python
+fn="prachi"
+fn="krish"
+print(fn[0])#k
+fn[0]="d"
+# print(fn[0])# 
+# TypeError: 'str' object does not support item assignment
+
+# program 3
+# does string stores the value by index?--yes
+
+# 0 1 2 3
+# p u n e
+
+
+cities=["pune","nagpur","wardha"]
+print(cities[0])
+print(cities[0])
+
