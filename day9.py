ab9ff8334f84c75b55aa3af28da8ca4471115201
DESKTOP-MPJJ6C3\Kinekar Vaishnavi <vaishnavikinekar11062@gmail.com>
2026-08-15 19:15:35 +0530

    add day9.py

---
+++ day9.py
@@ -0,0 +1,65 @@
+# print("hello".upper())
+# print("Hello".lower())
+# print("hello".capitalize())
+
+
+# # count()
+# fruit="banana"
+# print(fruit.count("a"))
+
+
+# # startswith(),endswith()
+# name="amol"
+# print(name.startswith("a"))
+# print(name.endswith("l"))
+
+
+# # count()
+# fruit="banana"
+# print(fruit.count("a"))
+
+# # startswith() , endswith()#returns True or False
+# name="Prachi"
+# print(name.startswith("Prac"))
+# print(name.startswith("P"))
+# print(name.endswith("i"))
+# print(name.endswith("achi"))
+
+
+# # find--it return index
+# skill="Programming"
+# print(skill.find("ram"))#4
+
+# # format
+# text="My name is {} and lastname is {}".format("Prachi","Kinekar")
+# # text="My name is {fname} and lastname is {lastname}".format(lastname="Kinekar",fname="Prachi")
+# print(text)
+
+
+# fn="Kiishu"
+# ln="Kinekar"
+# text5=f"My firstname is {fn} and lastname is{ln}"
+# print(text5)
+
+
+# # index
+# city="Pune"
+# print(city.index("u"))#1
+# print(city.index("Pune"))#0
+
+# Checked Methods
+print("abc".isalpha())#true
+print("1bc".isalpha())#False
+print("1234".isdecimal())#True
+print("1a".isalnum())#True
+print("1a#".isalnum())#False
+
+txt="python programming"
+print(txt.find("pro"))#7
+print("hello".islower())#True
+print("Hello".lower())#hello
+print("Hello".upper())#HELLO
+print("Hello".isupper())#false
+print("HELLO".isupper())#true
+
+
